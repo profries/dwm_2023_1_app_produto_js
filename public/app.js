93a49c22d02f5d867e1ce2f9ee88243e19453f84
LUIS HENRIQUE LEAL RIES <lhries@FPAP166748.local>
2023-05-06 16:43:20 -0300

    Adicionando timing na aplicacao para carregar produtos

--- public/app.js
+++ public/app.js
@@ -1,18 +1,27 @@
-//document.getElementById("produtos").innerHTML = "<h1>Lista de Produtos</h1>";
-let produto = {
-    id: 1,
-    nome: "Racao 1",
-    preco: 100.00,
-    imagem: "pedigree.jpg"
+//window.onload = function () {
+window.onload = () => {
+    carregaProduto();
+    setInterval(carregaProduto, 3000);
+};
+
+
+function carregaProduto() {
+
+    let produto = {
+        id: 1,
+        nome: "Racao 1",
+        preco: 100.00,
+        imagem: "pedigree.jpg"
+    }
+    document.querySelector("#produtos").innerHTML = `
+                <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
+                    <div class="w3-card">
+                        <div class="w3-container w3-center">
+                            <img src="${produto.imagem}" style="width: 70%">
+                            <h5>${produto.nome}</h5>
+                            <h3 class="w3-blue">${'R$ '+produto.preco.toLocaleString('pt-br',{minimumFractionDigits:2})}</h3>
+                        </div>
+                    </div>
+                </div>
+    `;
 }
-document.querySelector("#produtos").innerHTML = `
-            <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
-				<div class="w3-card">
-					<div class="w3-container w3-center">
-						<img src="${produto.imagem}" style="width: 70%">
-						<h5>${produto.nome}</h5>
-						<h3 class="w3-blue">${'R$ '+produto.preco.toLocaleString('pt-br',{minimumFractionDigits:2})}</h3>
-					</div>
-				</div>
-			</div>
-`;
